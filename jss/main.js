c7f7079047a6376f18a2e3c6e87c86f36b3e8396
simone-poli <polismn97@gmail.com>
2025-05-30 10:48:30 +0200

    snack1

---
+++ jss/main.js
@@ -0,0 +1,9 @@
+const firstNumber= Number(prompt('Inserisci primo numero'))
+
+const secondNumber= Number(prompt('Inserisci secondo numero'))
+
+if (firstNumber > secondNumber){
+    console.log ('Il numero maggiore è:', firstNumber)
+}else{
+    console.log ('Il numero maggiore è:', secondNumber)
+}
